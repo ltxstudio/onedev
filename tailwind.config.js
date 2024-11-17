853d41d96281e8a59378720bdc7c0b7da1c5e039
Ltxstudio <ltxstudio.info@gmail.com>
2024-11-17 23:08:45 +0600

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,18 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#3498db",
+        secondary: "#2ecc71",
+        dark: "#2c3e50",
+        light: "#ecf0f1",
+      },
+    },
   },
   plugins: [],
 }
