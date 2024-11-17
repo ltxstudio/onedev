81f84604e0adff7a08f43b037d8bf95ed9c42be6
Ltxstudio <ltxstudio.info@gmail.com>
2024-11-17 23:37:19 +0600

    Update AnimatedBackground.jsx

--- src/components/AnimatedBackground.jsx
+++ src/components/AnimatedBackground.jsx
@@ -1,19 +1,21 @@
 import React from 'react';
 import { Canvas } from '@react-three/fiber';
-import { Stars } from '@react-three/drei';
+import { OrbitControls, Stars } from '@react-three/drei';
 
 const AnimatedBackground = () => {
   return (
     <div className="fixed inset-0 z-[-1]">
       <Canvas>
+        {/* Starry Sky */}
         <Stars
           radius={100}
           depth={50}
           count={5000}
           factor={4}
-          saturation={0}
           fade
         />
+        {/* Smooth movement and slight rotation */}
+        <OrbitControls enableZoom={false} autoRotate autoRotateSpeed={0.5} />
       </Canvas>
     </div>
   );
