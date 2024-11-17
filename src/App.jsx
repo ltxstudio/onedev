7e10a0780ca4f8c2dae03c008c7504d46cceb193
Ltxstudio <ltxstudio.info@gmail.com>
2024-11-18 02:01:28 +0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -13,14 +13,20 @@ import NotFound from './pages/NotFound';
 
 const App = () => {
   return (
-    <div>
-     <Home />
-     <About />
-     <Services />
-<Testimonials />
-<Blog />
-<Portfolio />
-
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/portfolio" element={<Portfolio />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/blog/:postId" element={<BlogPost />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/testimonials" element={<Testimonials />} />
+        <Route path="/resume" element={<Resume />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Router>
   );
 };
 
