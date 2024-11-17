4ec3bf56c2c1f3f4c0173ccfdae98e1b7ab8c245
Ltxstudio <ltxstudio.info@gmail.com>
2024-11-17 23:20:36 +0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,17 +6,25 @@ import Blog from './pages/Blog';
 import BlogPost from './pages/BlogPost';
 import Contact from './pages/Contact';
 import About from './pages/About';
+import Testimonials from './pages/Testimonials';
+import Resume from './pages/Resume';
+import NotFound from './pages/NotFound';
+import AnimatedBackground from './components/AnimatedBackground';
 
 const App = () => {
   return (
     <Router>
+      <AnimatedBackground />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/portfolio" element={<Portfolio />} />
         <Route path="/blog" element={<Blog />} />
-        <Route path="/blog/:postId" element={<BlogPost title="Blog Title" content="Here goes the content of the blog post." />} />
+        <Route path="/blog/:postId" element={<BlogPost />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/about" element={<About />} />
+        <Route path="/testimonials" element={<Testimonials />} />
+        <Route path="/resume" element={<Resume />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
   );
